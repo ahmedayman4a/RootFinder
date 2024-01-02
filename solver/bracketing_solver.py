1335c77e2f597894b95f0836b77c5bf8bce8812d
Ahmed Ayman <ahmedayman4a77@gmail.com>
2024-01-02 16:08:57 +0200

    Change steps in bracketing solver

--- solver/bracketing_solver.py
+++ solver/bracketing_solver.py
@@ -1,4 +1,4 @@
-from round_to_sf import round_to_sf
+from solver.round_to_sf import round_to_sf
 
 class BracketingMethodsSolver:
     def __init__(self, equation, sf):
@@ -19,14 +19,14 @@ class BracketingMethodsSolver:
             raise ValueError("Bisection method fails.")
         else:
             x_old = round_to_sf((L + U) / 2, self.sf)
-            self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_old}\n")
+            self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_old}\n")
             if self.equation(L) * self.equation(x_old) < 0:
                 U = x_old
             else:
                 L = x_old
             x_new = round_to_sf((L + U) / 2, self.sf)
             step += 1
-            self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_new} Absolute Error: {self.absolute_error(x_new, x_old)}\n")
+            self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_new}\nAbsolute Error: {self.absolute_error(x_new, x_old)}\n")
             while self.absolute_error(x_new, x_old) > error and step < 100:
                 x_old = x_new
                 if self.equation(L) * self.equation(x_old) < 0:
@@ -35,7 +35,7 @@ class BracketingMethodsSolver:
                     L = x_old
                 x_new = round_to_sf((L + U) / 2, self.sf)
                 step += 1
-                self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_new} Absolute Error: {self.absolute_error(x_new, x_old)}\n")
+                self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_new}\nAbsolute Error: {self.absolute_error(x_new, x_old)}\n")
             self.steps.append(f"X: {x_new}")
             return x_new
 
@@ -49,14 +49,14 @@ class BracketingMethodsSolver:
             raise ValueError("False position method fails.")
         else:
             x_old = round_to_sf((L * self.equation(U) - U * self.equation(L)) / (self.equation(U) - self.equation(L)), self.sf)
-            self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_old}\n")
+            self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_old}\n")
             if self.equation(L) * self.equation(x_old) < 0:
                 U = x_old
             else:
                 L = x_old
             x_new = round_to_sf((L * self.equation(U) - U * self.equation(L)) / (self.equation(U) - self.equation(L)), self.sf)
             step += 1
-            self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_new} Absolute Error: {self.absolute_error(x_new, x_old)}\n")
+            self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_new}\nAbsolute Error: {self.absolute_error(x_new, x_old)}\n")
             while self.absolute_error(x_new, x_old) > error and step < 100:
                 x_old = x_new
                 if self.equation(L) * self.equation(x_old) < 0:
@@ -65,15 +65,15 @@ class BracketingMethodsSolver:
                     L = x_old
                 x_new = round_to_sf((L * self.equation(U) - U * self.equation(L)) / (self.equation(U) - self.equation(L)), self.sf)
                 step += 1
-                self.steps.append(f"[{step}]: Lower bound: {L} Upper bound: {U} X: {x_new} Absolute Error: {self.absolute_error(x_new, x_old)}\n")
+                self.steps.append(f"[{step}]:\nLower bound: {L}\nUpper bound: {U}\nX: {x_new}\nAbsolute Error: {self.absolute_error(x_new, x_old)}\n")
             self.steps.append(f"X: {x_new}")
             return x_new
 
 # Example:
-equation = lambda x: 3 * x**4 + 6.1 * x**3 - 2 * x**2 + 3 * x + 2
-sf = 5  # Specify the desired number of significant figures
-solver = BracketingMethodsSolver(equation, sf)
-bisection_result = solver.bisection_method(-1, 0, 0.001)
-print("Bisection Method Result For a:", bisection_result)
-false_position_result = solver.false_position_method(-1, 0, 0.001)
-print("False Position Method Result For a:", false_position_result)
+# equation = lambda x: 3 * x**4 + 6.1 * x**3 - 2 * x**2 + 3 * x + 2
+# sf = 5  # Specify the desired number of significant figures
+# solver = BracketingMethodsSolver(equation, sf)
+# bisection_result = solver.bisection_method(-1, 0, 0.001)
+# print("Bisection Method Result For a:", bisection_result)
+# false_position_result = solver.false_position_method(-1, 0, 0.001)
+# print("False Position Method Result For a:", false_position_result)
